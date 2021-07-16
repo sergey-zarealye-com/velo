aa87d3e344b1c7525ceea89063f8998300e87d24
Александр Макаров <makarov.alxr@yandex.ru>
2021-07-16 14:47:10 +0700

    proto of processing funcion

--- video-handler/main.py
+++ video-handler/main.py
@@ -2,13 +2,39 @@ from connectors import Connector
 import yaml  # type: ignore
 import argparse
 import time
+import os
+import subprocess
+import re
 
 
 async def processing_function(
     request: dict,
 ):
-    # TODO process request
     print("Got request:", request)
+
+    thumbs_dir = request['thumbs_dir']
+    input_fname = request['input_fname']
+    input_fname_stem = request['input_fname_stem']
+    img_ext = request['img_ext']
+
+    out = f"{os.path.join(thumbs_dir, f'{input_fname_stem}_frame_' + '%0d' + img_ext)}"
+    command = f"""ffmpeg -y -i {str(input_fname)} -vsync vfr -filter_complex "[0:v]select=eq(pict_type\,PICT_TYPE_I)[pre_thumbs];[pre_thumbs]select=gt(scene\,0.2),scale=256:256[thumbs]" -map [thumbs] {out} 2>&1"""
+
+    process = subprocess.Popen(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        encoding='utf8'
+    )
+    (output, err) = process.communicate()
+    files = []
+    for file in os.listdir(thumbs_dir):
+        m = re.match(f'{input_fname.stem}_frame_\d+{img_ext}', file)
+        if m:
+            file_path = os.path.join(thumbs_dir, file)
+            files.append(file_path)
+    
+    # TODO write to database
 
 
 def main():
