732e3329ba5a619df12a663871e9e8ec2b9e0512
alexander <a.barmin@napoleonit.ru>
2021-07-31 18:47:38 +0300

    feat: обновляение классов в БД после нажатия apply filter

--- project/images/views.py
+++ project/images/views.py
@@ -4,8 +4,8 @@
 import json
 import math
 import ntpath
-import os
 from collections import Counter
+from typing import Dict
 
 from flask import render_template, Blueprint, redirect, url_for, flash, request
 from flask import abort, session, send_from_directory
@@ -14,16 +14,15 @@ from markupsafe import Markup
 
 from project import app, db
 from project.datasets.queries import get_nodes_above
-from project.images.queries import get_items_of_version, get_uncommited_items
-from project.models import Version, Category
+from project.images.queries import get_items_of_version, get_uncommited_items, uncommited_items_filter, \
+    update_uncommited_items, get_id_by_name
+from project.models import Version, Category, VersionItems
 
 # CONFIG
 images_blueprint = Blueprint('images', __name__,
                              template_folder='templates',
                              url_prefix='/images')
 
-
-# TODO: сделать логгер, вынести в него все принты
 
 # ROUTES
 @images_blueprint.route('/index')
@@ -48,11 +47,46 @@ def download_file(filename):
     return send_from_directory(head, tail, as_attachment=True)
 
 
+def split_items(data) -> Dict:
+    """Разделяет data items на закомиченные и не закомиченные"""
+    item_ids = list(map(int, list(data.get('moderated_items').keys())))
+    uncommited = uncommited_items_filter(db.session, item_ids)
+    commited = [id for id in item_ids if id not in uncommited]
+    splited = {
+        "commited": {id: data.get('moderated_items').get(str(id)) for id in commited},
+        "uncommited": {id: data.get('moderated_items').get(str(id)) for id in uncommited}
+    }
+    return splited
+
+
 @images_blueprint.route('/save_changes', methods=['POST'])
 def save_changes():
+    """Записывает в БД изменения классов"""
     if request.method == "POST":
-        # TODO: this data to update DB
-        print(request.json)
+        data = request.json
+        if "moderated_items" in data:
+            splitted_items = split_items(data)
+            try:
+                uncommited = splitted_items.get("uncommited")
+                if len(uncommited):
+                    # Незакомиченные записи обновляются в таблице TmpTable
+                    update_uncommited_items(db, uncommited)
+                commited = splitted_items.get("commited")
+                if len(commited):
+                    # Закомиченные записи не изменяются - в таблицу version items добавляются
+                    # новые записи
+                    node_id = get_id_by_name(data['node_name'])
+                    objects = [VersionItems(item_id=item_id,
+                                            version_id=node_id,
+                                            category_id=int(moderation['cl']))
+                               for item_id, moderation in commited.items()]
+                    db.session.bulk_save_objects(objects)
+                db.session.commit()
+            except Exception as ex:
+                db.session.rollback()
+                message = Markup(
+                    "<strong>Error!</strong> Unable to commit changes " + str(ex))
+                flash(message, 'danger')
     return "Ok"
 
 
@@ -83,13 +117,11 @@ def browse(selected, page=1, items=50, filters=None):
         return redirect(url_for('datasets.index'))
     nodes_of_version = get_nodes_above(db.session, version.id)
     version_items = get_items_of_version(db.session, nodes_of_version)
-    # TODO: отобразить на фронте
     uncommitted_items = get_uncommited_items(db.session, selected)
-
     # get vision classes
     classes_info = {cl.name: {'id': cl.id, 'amount': 0} for cl in Category.list(Category.TASKS()[0][0], version.name)}
     # count items per class in current ds
-    cur_ds_info = dict(Counter(getattr(item, 'label') for item in version_items+uncommitted_items))
+    cur_ds_info = dict(Counter(getattr(item, 'label') for item in version_items + uncommitted_items))
     # map vision classes with cur_ds_info
     for key, value in cur_ds_info.items():
         if key in classes_info:
@@ -99,7 +131,7 @@ def browse(selected, page=1, items=50, filters=None):
     # if "browse_filters" exists in session
     if "browse_filters" in session:
         version_items_filter = []
-        cur_filters = {"uncommitted": False, "committed": False }
+        cur_filters = {"uncommitted": False, "committed": False}
         # show only uncommitted items
         if "uncommitted" in session['browse_filters'] and session['browse_filters']["uncommitted"]:
             version_items_filter += uncommitted_items
@@ -108,8 +140,9 @@ def browse(selected, page=1, items=50, filters=None):
             version_items_filter += version_items
             cur_filters["committed"] = True
         # filter by class
-        if "class_filter" in session['browse_filters']: # and len(session['browse_filters']["class_filter"]):
-            version_items_filter = [item for item in version_items_filter if item.label in session['browse_filters']["class_filter"]]
+        if "class_filter" in session['browse_filters']:  # and len(session['browse_filters']["class_filter"]):
+            version_items_filter = [item for item in version_items_filter if
+                                    item.label in session['browse_filters']["class_filter"]]
             cur_filters["class_filter"] = session['browse_filters']["class_filter"]
             if len(classes_info) != len(cur_filters["class_filter"]):
                 cb_all_cl_filters = False
@@ -122,11 +155,11 @@ def browse(selected, page=1, items=50, filters=None):
                            version=version,
                            version_items=version_items_filter[(page - 1) * items:(page) * items],
                            ds_length={
-                               "all": len(version_items)+len(uncommitted_items),
+                               "all": len(version_items) + len(uncommitted_items),
                                "committed": len(version_items),
                                "uncommitted": len(uncommitted_items)
                            },
-                           cb_all_cl_filters = cb_all_cl_filters,
+                           cb_all_cl_filters=cb_all_cl_filters,
                            classes_info=classes_info,
                            pages=int(math.ceil(len(version_items_filter) / int(items))),
                            page=page,
